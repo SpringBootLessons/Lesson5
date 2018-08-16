0ff748fc7458e8291a51716fd56defab0e73ecb2
Mona Lisa <josemyjoseph@gmail.com>
2018-08-16 11:58:09 -0400

    initial commit

---
+++ src/main/java/com/example/lesson5/Song.java
@@ -0,0 +1,60 @@
+package com.example.lesson5;
+
+public class Song {
+//  Java Bean
+//  This class is going to be a blueprint for all of the objects created for that class.
+  private long id;
+  private String name;
+  private String artist;
+  private String album;
+  private int rating;
+  private int year;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getArtist() {
+    return artist;
+  }
+
+  public void setArtist(String artist) {
+    this.artist = artist;
+  }
+
+  public String getAlbum() {
+    return album;
+  }
+
+  public void setAlbum(String album) {
+    this.album = album;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public void setRating(int rating) {
+    this.rating = rating;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+}
